9c10cfb22b07e696113bc8bc2bdbce768bee022f
Bardan Neupane <nbardan10@gmail.com>
2025-05-06 12:47:39 +0545

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/testsite/', // Replace 'repository-name' with your repo's name
+  build: {
+    outDir: 'dist',  // The folder Vite will use to store the built files
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
